76bfe77e0945e264483a5ff068ee75625998ebbf
Rioh <90512887+Rioh1118@users.noreply.github.com>
2024-06-26 11:35:20 +0900

    db initial

--- api/shuttle/src/main.rs
+++ api/shuttle/src/main.rs
@@ -1,16 +1,12 @@
 use actix_web::{get, web::ServiceConfig};
 use shuttle_actix_web::ShuttleActixWeb;
 
-#[get("/")]
-async fn hello_world() -> &'static str {
-    "raach"
-}
-
 #[shuttle_runtime::main]
-async fn main() -> ShuttleActixWeb<impl FnOnce(&mut ServiceConfig) + Send + Clone + 'static> {
+async fn actix_web(
+    #[shuttle_shared_db::Postgres()] pool: sqlx::PgPool,
+) -> ShuttleActixWeb<impl FnOnce(&mut ServiceConfig) + Send + Clone + 'static> {
     let config = move |cfg: &mut ServiceConfig| {
         cfg.service(hello_world);
     };
-
     Ok(config.into())
 }
